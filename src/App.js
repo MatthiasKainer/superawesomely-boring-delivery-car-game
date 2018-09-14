dce45680c55d96a505482338ed83f81b8be21e25
Matthias Kainer <mail@matthias-kainer.de>
2018-09-14 20:43:53 +0200

    Added touchevents and another level

--- src/App.js
+++ src/App.js
@@ -7,6 +7,79 @@ import Player from "./core/assets/player.png";
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        document.onkeydown = (e) => this.handleKeyDown(e);
+        this.swipedetect(document.getElementById('root'), (swipedir) => {
+            this.handleSwipe(swipedir);
+        });
+    }
+
+    swipedetect(el, callback) {
+        if (!el) return;
+        let touchsurface = el,
+            swipedir,
+            startX,
+            startY,
+            distX,
+            distY,
+            threshold = 150, //required min distance traveled to be considered swipe
+            restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+            allowedTime = 300, // maximum time allowed to travel that distance
+            elapsedTime,
+            startTime,
+            handleswipe = callback || function (swipedir) { }
+
+        touchsurface.addEventListener('touchstart', (e) => {
+            const touchobj = e.changedTouches[0];
+            swipedir = 'none';
+            startX = touchobj.pageX;
+            startY = touchobj.pageY;
+            startTime = new Date().getTime(); // record time when finger first makes contact with surface
+            e.preventDefault();
+        }, false)
+
+        touchsurface.addEventListener('touchmove', (e) => {
+            e.preventDefault(); // prevent scrolling when inside DIV
+        }, false)
+
+        touchsurface.addEventListener('touchend', (e) => {
+            const touchobj = e.changedTouches[0];
+            distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+            distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+            elapsedTime = new Date().getTime() - startTime; // get time elapsed
+            if (elapsedTime <= allowedTime) { // first condition for awipe met
+                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+                    swipedir = (distX < 0) ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
+                }
+                else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+                    swipedir = (distY < 0) ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
+                }
+            }
+            handleswipe(swipedir);
+            e.preventDefault();
+        }, false);
+    }
+
+    handleSwipe(swipedir) {
+        const { movePlayer } = this.props;
+        if (!movePlayer) return;
+        // swipedir contains either "none", "left", "right", "top", or "down"
+        console.log(`Swiping ${swipedir}`);
+        switch (swipedir) {
+            case "left":
+                return movePlayer(Direction.LEFT);
+            case "up":
+                return movePlayer(Direction.UP);
+            case "right":
+                return movePlayer(Direction.RIGHT);
+            case "down":
+                return movePlayer(Direction.DOWN);
+            default:
+                return false;
+        }
+    }
+
     handleKeyDown(e) {
         const { movePlayer } = this.props;
         if (!movePlayer) return;
@@ -24,7 +97,6 @@ class App extends Component {
         }
     }
     render() {
-        document.onkeydown = (e) => this.handleKeyDown(e);
         return (
             <div className="App">
                 <header className="App-header">
--- src/data/levels/index.js
+++ src/data/levels/index.js
@@ -2,6 +2,7 @@ const tutorial = require("./tutorial.json");
 const level1 = require("./level1.json");
 const level2 = require("./level2.json");
 const level3 = require("./level3.json");
+const level4 = require("./level4.json");
 const none = {};
 
-export { tutorial, level1, level2, level3, none };
+export { tutorial, level1, level2, level3, level4, none };
